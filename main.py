8f101c1a0aae958b53d335a4372cc1fde8b90e02
tamipramos <tamipramos@gmail.com>
2023-02-04 22:31:57 +0000

    Corregido fallo de inicialización

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from modules.functions.main import client, files
-import sys, time
+import sys, time, os
 import modules.gui.gui as gui
 
 ###==========================================================================================================================###
@@ -10,12 +10,19 @@ import modules.gui.gui as gui
 current_dir=sys.path[0]
 
 #grabs config file
-data = eval(open(current_dir+'/config/settings.json', 'r').read())
+try:
+    data = eval(open(current_dir+'/config/settings.json', 'r').read())
+except:
+    #Write files
+    os.mkdir('./config')
+    files.Files.create_startup_file()
+    data = eval(open(current_dir+'/config/settings.json', 'r').read())
 
 #defines variable for each configuration
 global serverConf, serverProperties
 serverConf = data['serverConfig']
 serverProperties = data['serverProperties']
+
 
 #current setting=NULL
 #curr={}
@@ -41,6 +48,7 @@ def update_config_files():
 
 #create main files
 F=files.Files(current_dir+'/config', serverProperties, serverConf)
+
 #F.editConfig(file=F.sshConfigurationDestination, old="server-ip", new="jejeje")
 #Start App  
 app=gui.Main(serverConf, F)
